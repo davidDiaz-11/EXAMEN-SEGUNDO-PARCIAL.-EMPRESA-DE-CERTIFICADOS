05ca7001e0d8cd4a1164a0061e5e1c048007f3c1
davidDiaz-11 <al347754@edu.uaa.mx>
2025-10-31 15:06:23 -0600

    Levante el servidor

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-const express = requiere("")
+const express = require("express");
+const cors = require('cors');
+const authRoutes = require("./routes/auth.routes");
+
+
+const app = express();
+const PORT = 3000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+// Ruta base
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando correctamente 🚀');
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
